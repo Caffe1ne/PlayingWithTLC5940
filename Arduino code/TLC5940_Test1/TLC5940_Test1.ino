ec9bd7e7b587f84a789dc59280c953877e9bfe3a
Karl Fredrik Haugland <Karl.Fredrik.Haugland@gmail.com>
2014-01-08 02:41:29 +0100

    Out with the old code and comments

--- Arduino code/TLC5940_Test1/TLC5940_Test1.ino
+++ Arduino code/TLC5940_Test1/TLC5940_Test1.ino
@@ -1,9 +1,10 @@
 // YES THIS FILE IS MESSY I KNOW
 
-#include "Tlc5940.h"
-#define DEBUG 1 // 0 == off, 1 == errors, 2 == all the things
-#define NUMBER_OF_LEDS 5 // Number of LEDS in use
+#include <Tlc5940.h>
+#define DEBUG 2 // 0 == off, 1 == errors, 2 == all the things
+#define NUMBER_OF_LEDS 21 // Number of LEDS in use
 #define MAX_PWM_VALUE 4095 // Maximum value of PWM driver. For TLC5940s that's 4095
+#define potValue 4095
 
 float pi = 22/7; // Self-explainatory
 
@@ -17,72 +18,26 @@ int chanRed[NUM_TLCS*16/3];
 int chanGreen[NUM_TLCS*16/3];
 int chanBlue[NUM_TLCS*16/3];
 int sizeOfArrays= (NUM_TLCS*16) - (NUM_TLCS*16)%3; // Make sure the array's fit for RGB LEDs and not any accidental RG LEDs
+//int potValue; // ...and the potmeter for speed control
 
-int redTest = 0;
-int greenTest = 2048;
-int blueTest = 0;
-
-
-void setup()
-{
+void setup(){
   Serial.begin(9600);
+  Serial.print("Number of TLCs: ");
   Serial.println(NUM_TLCS);
-  Serial.println(sizeOfArrays);
-  
+
   Tlc.init();
   Tlc.clear();
-  
-  for(int i = 0; i < 5; i++){
-    setColorLED(i, redTest, greenTest, blueTest);
-  }
-  updateColors();  
 }
 
-void setColorLED(int numLED, int red, int green, int blue){
-  chanRed[numLED] = red;
-  chanGreen[numLED] = green;
-  chanBlue[numLED] = blue;
-  
-  if(DEBUG > 1){
-    Serial.print("LED number ");
-    Serial.print(numLED);
-    Serial.print(" now has RGB values ");
-    Serial.print(red);
-    Serial.print(", ");
-    Serial.print(green);
-    Serial.print(", ");
-    Serial.println(blue);
-  }
-}
-
-void updateColors(){
-  Tlc.clear();  // Clear out the old values
-//  delay(20);  // Give it time to clear up
-
-  /* Push values from arrays to the TLC library */ // Until I dig into the lib itself and find a cleaner way to do that...
-  for(int i = 0; i < sizeOfArrays; i+=3){
-    Tlc.set(i, chanRed[i/3]);
-    Tlc.set(i+1, chanGreen[i/3]);
-    Tlc.set(i+2, chanBlue[i/3]);
-  }
-
-//  delay(20); // Think for a bit
-  Tlc.update(); // Write values to chips and apply them
-}
-
-void loop()
-{
-  for(float i = 0; i < 360; i+=5.0){
-    for(int y = 0; y < NUMBER_OF_LEDS; y++){
-      setColorLED(y, sinRed(i+(360/NUMBER_OF_LEDS*y)), sinGreen(i+(360/NUMBER_OF_LEDS*y)), sinBlue(i+(360/NUMBER_OF_LEDS*y)));
-    }
-    
-    /*for(int y = 0; y < NUMBER_OF_LEDS; y++){
-      setColorLED(y, sinRed(i), sinGreen(i), sinBlue(i));
-    }*/
-    
-    updateColors();
-    delay(10);
+void loop(){
+ for(float i = 1; i > 0; i -= getSpeed()){
+//    potValue = map(analogRead(A0), 0, 1024, 0, MAX_PWM_VALUE);
+  for(int y = 0; y < NUMBER_OF_LEDS; y++){
+    float offset = (1.0/NUMBER_OF_LEDS)*y;
+    setColorLED(y, sinRed(i+offset, y), sinGreen(i+offset, y), sinBlue(i+offset, y));
   }
   
+  updateColors();
+  delay(1);
+  }
 }
--- Arduino code/TLC5940_Test1/functions.ino
+++ Arduino code/TLC5940_Test1/functions.ino
@@ -1,27 +1,73 @@
+// Float mapping!
+float map_double(double x, double in_min, double in_max, double out_min, double out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
-/**************************
- * RGB FUNCTION SELECTION *
- **************************/
- 
-/* Uncomment the function you want to use and comment out the other two */
-        /***** BEGIN PLAIN CUTOFF FUNCTION *****/
-        /***** END PLAIN CUTOFF FUNCTION *****/
+// Calculate the various colors
+int sinRed(float x, int y){
+  float valUnconstrained = ((2 * 1.5) / pi * asin(sin(2 * pi / 1 * x + (pi / 2))) + 0.5);
+  float val = constrain(valUnconstrained, 0.0, 1.0);
+  if(y == 0){
+    Serial.print(x);
+    Serial.print(",");
+    Serial.print(val);
+    Serial.print(",");
+    Serial.print(map_double(val, 0.0, 1.0, 0.0, 4095.0));
+  }
+  return map_double(val, 0.0, 1.0, 0, potValue);
+}
 
-        /***** BEGIN CUTOFF SINOID FUNCTION ****
-        /***** END CUTOFF SINOID FUNCTION *****/
+int sinGreen(float x, int y){
+  float valUnconstrained = ((2 * 1.5) / pi * asin(sin(2 * pi / 1 * x - (pi / 6))) + 0.5);
+  float val = constrain(valUnconstrained, 0.0, 1.0);
+   if(y == 0){
+    Serial.print(",");
+    Serial.print(map_double(val, 0.0, 1.0, 0, potValue));
+   }
+  return map_double(val, 0.0, 1.0, 0, potValue);
+}
+
+int sinBlue(float x, int y){
+  float valUnconstrained = ((2 * 1.5) / pi * asin(sin(2 * pi / 1 * x - 5 * (pi / 6))) + 0.5);
+  float val = constrain(valUnconstrained, 0.0, 1.0);
+   if(y == 0){
+    Serial.print(",");
+    Serial.println(map_double(val, 0.0, 1.0, 0, potValue));
+   }
+  return map_double(val, 0.0, 1.0, 0, potValue);
+}
 
 
-// NJSG        /***** BEGIN SINOID FUNCTION *****/
-        // This is a medicore function which is rather obsolete but it's what I've got currently
-        // These functions are called using the iteration of the control function/loop and these control the value based on that
-        // They currently take a float which is supposed to be in the range 0-360 (following nice circular sinoids)
-        int sinRed(float x){
-          return (MAX_PWM_VALUE/2 * sin((x * pi) / 180 + offsetRed) + MAX_PWM_VALUE/2);
-        }
-        int sinGreen(float x){
-          return (MAX_PWM_VALUE/2 * sin((x * pi) / 180 + offsetGreen) + MAX_PWM_VALUE/2);
-        }
-        int sinBlue(float x){
-          return (MAX_PWM_VALUE/2 * sin((x * pi) / 180 + offsetBlue) + MAX_PWM_VALUE/2);
-        }
-        /***** END SINOID FUNCTION *****/
+void setColorLED(int numLED, int red, int green, int blue){
+  chanRed[numLED] = red;
+  chanGreen[numLED] = green;
+  chanBlue[numLED] = blue;
+  
+  if(DEBUG > 2){
+    Serial.print("LED number ");
+    Serial.print(numLED);
+    Serial.print(" now has RGB values ");
+    Serial.print(red);
+    Serial.print(", ");
+    Serial.print(green);
+    Serial.print(", ");
+    Serial.println(blue);
+  }
+}
+
+void updateColors(){
+  Tlc.clear();  // Clear out the old values
+  /* Push values from arrays to the TLC library */ // Until I dig into the lib itself and find a cleaner way to do that...
+  for(int i = 0; i < sizeOfArrays; i+=3){
+    Tlc.set(i, chanRed[i/3]);
+    Tlc.set(i+1, chanGreen[i/3]);
+    Tlc.set(i+2, chanBlue[i/3]);
+  }
+  Tlc.update(); // Write values to chips and apply them
+}
+
+float getSpeed(){
+  float temp = map (analogRead(A1), 20, 1020, 0, 1000);
+  temp = 0.00005 * temp;
+  return temp;
+}
